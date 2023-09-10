7be9339a90a9763ca96a82eef26e1bbdc3d5ffa0
Chamika Edirisinghe <edirisinghe.chamika.madushan@gmail.com>
2023-09-10 19:04:33 +0530

    Completed day 007 task

---
+++ Day 007 - Java End-of-file/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int i = 1;
+        while (scanner.hasNext()) {
+            System.out.printf("%d %s%n", i++, scanner.nextLine());
+        }
+        
+        scanner.close();
+        
+    }
+}
